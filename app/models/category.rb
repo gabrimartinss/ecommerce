dda66847b1dab49708f31902c42e8c939bf50c10
gabrimartinss <gabriel.martinsdesouza2@gmail.com>
2024-11-14 20:30:53 -0300

    refactor: category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,13 @@
 class Category < ApplicationRecord
+  # Adicione validações se necessário
+  validates :name, presence: true
+  
+  # Para debug
+  after_initialize do |category|
+    Rails.logger.debug "New category initialized: #{category.attributes}"
+  end
+
+  before_save do |category|
+    Rails.logger.debug "About to save category: #{category.attributes}"
+  end
 end
