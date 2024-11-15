d4bac32dcd07b1ce8fedb79d2d249a22f51ba436
gabrimartinss <gabriel.martinsdesouza2@gmail.com>
2024-11-14 21:32:23 -0300

    feat: image processing

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -56,6 +56,6 @@ class Admin::CategoriesController < AdminController
   end
 
   def category_params
-    params.require(:category).permit(:name, :description)
+    params.require(:category).permit(:name, :description, :image)
   end
 end
